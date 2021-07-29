93bab4573a75b7f3fa04e61a5bf85f4f833c6ea0
elmasha <elmashavitran@gmail.com>
2021-07-29 09:31:03 +0300

    12

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,51 +1,54 @@
 <template>
   <div>
-    <h4 class="" align="center">SwiftGas DashBoard</h4>
-    <br />
+    <h4 class="">SwiftGas DashBoard</h4>
     <br />
     <div class="container">
-      <div>
-        <b-container class="bv-example-row">
-          <b-row>
-            <b-col cols="12" md="8">
-              <div class="flex-start">
-                <h6>Active Shops</h6>
-                <k-progress
-                  :value="25"
-                  variant="success"
-                  :percent="45"
-                  :striped="striped"
-                  line-height="13"
-                  active="true"
-                  color="#242a37"
-                  active-color="#ff8900"
-                ></k-progress>
-                <h6>InActive Shops</h6>
-                <k-progress
-                  value="45%"
-                  line-height="13"
-                  percent="100"
-                  color="#242a37"
-                  active="true"
-                  show-progress
-                  active-color="#ff8900"
-                ></k-progress>
-              </div>
-              <br />
-              <br />
-              <div class="container">
-                <div class="col-md-12">
-                  <div class="row">
-                    <span
-                      ><h2>{{ Active }}</h2></span
-                    >
-                  </div>
+      <div class="col-md-12">
+        <div class="col-md-6">
+          <h6>Active Shops</h6>
+          <k-progress
+            :value="25"
+            variant="success"
+            :percent="totalPercent"
+            :striped="striped"
+            line-height="13"
+            active="true"
+            color="#242a37"
+            active-color="#ff8900"
+          ></k-progress>
+          <h6>InActive Shops</h6>
+          <k-progress
+            value="45%"
+            line-height="13"
+            percent="100"
+            color="#242a37"
+            active="true"
+            show-progress
+            active-color="#ff8900"
+          ></k-progress>
+        </div>
+
+        <div class="col-md-6">
+          <div class="container">
+            <div class="">
+              <div class="container-fluid container-xl d-flex container col-md-12"></div>
+              <div class="row">
+                <div class="col-md-3">
+                  <h2>{{ active }}</h2>
+                </div>
+                <div class="col-md-3">
+                  <h2>{{ inactive }}</h2>
+                </div>
+                <div class="col-md-3">
+                  <h2>{{ noOfshops }}</h2>
+                </div>
+                <div class="col-md-3">
+                  <h2>{{ noOfOrders }}</h2>
                 </div>
               </div>
-            </b-col>
-            <b-col cols="6" md="4"></b-col>
-          </b-row>
-        </b-container>
+            </div>
+          </div>
+        </div>
       </div>
 
       <div class="fixed-action-btn">
@@ -63,16 +66,28 @@ export default {
   data() {
     return {
       value: null,
-      active: null,
-      inactive: null,
-      noOfshops: null,
+      active: 0,
+      inactive: 0,
+      noOfshops: 0,
       noOfCustomers: null,
-      noOfOrders: null,
+      noOfOrders: 0,
+      totalPercent: 0,
+
       percentActive: null,
       percentInactive: null,
       max: 100,
       data1: [],
     };
+  },
+  created() {
+    this.ActiveStatus;
+  },
+
+  computed() {
+    this.ActiveStatus;
+  },
+  beforeUpdate() {
+    this.ActiveStatus;
   },
 
   mounted() {
@@ -81,18 +96,21 @@ export default {
       .get()
       .then((queryResult) => {
         console.log(queryResult.data());
-        const data = {
-          id: queryResult.id,
-          Active: queryResult.data().Active_Shops,
-          Inactive: queryResult.data().Inactive_shops,
-          NoOrders: queryResult.data().No_of_Orders,
-          NoCustomers: queryResult.data().Registered_Customers,
-          NoVendors: queryResult.data().Registered_Vendors,
-          NoShops: queryResult.data().Total_No_Shops,
-        };
-        console.log(data);
-        this.data1.push(data);
+
+        (this.active = queryResult.data().Active_Shops),
+          (this.inactive = queryResult.data().Inactive_shops),
+          (this.noOfOrders = queryResult.data().No_of_Orders),
+          (this.noOfCustomers = queryResult.data().Registered_Customers),
+          (this.noOfshops = queryResult.data().Registered_Vendors),
+          (this.noOfshops = queryResult.data().Total_No_Shops);
       });
+  },
+  methods: {
+    ActiveStatus() {
+      var average = this.active / this.noOfshops;
+      this.totalPercent = average * 100;
+      console.log(this.totalPercent);
+    },
   },
 };
 </script>
